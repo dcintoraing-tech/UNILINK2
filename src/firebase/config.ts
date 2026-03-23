12eb76a8d24db5bba65f48b709680782361b6dd8
Daniel Cintora <dcintora.ing@gmail.com>
2026-03-23 02:32:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9397941301-d9e64",
+  "appId": "1:280811439859:web:a27d4d3a0cc6e5b97c1986",
+  "apiKey": "AIzaSyA3XvVDN_BCGkt4Xyp1B_2fukDToLH5VoM",
+  "authDomain": "studio-9397941301-d9e64.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "280811439859"
+};
