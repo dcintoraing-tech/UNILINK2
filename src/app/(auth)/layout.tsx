7fe579293050375fb7cae0a77dab09e69ebc5b5f
Daniel Cintora <dcintora.ing@gmail.com>
2026-04-23 04:21:26 +0000

    al doble

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({
     <main className="flex min-h-screen w-full flex-col items-center justify-center p-4">
       <div className="w-full max-w-sm">
         <div className="mb-8 flex justify-center">
-            <Logo className="h-40 w-40" />
+            <Logo className="h-80 w-80" />
         </div>
         {children}
       </div>
